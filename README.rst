6f02d7137369efe486f847f52766195c9c0ce7bf
mauro-balades <mauro.balades@tutanota.com>
2022-10-29 11:57:56 +0200

    added CLI entry point

---
+++ README.rst
@@ -0,0 +1,4 @@
+Breeze
+=======
+
+Breeze is a light-weight, highly-customizable build tool for different languages such as C(++) and Java
--- setup.py
+++ setup.py
@@ -11,11 +11,20 @@ setup(
     version="0.1.0",
     description="Breeze is a light-weight, highly-customizable build tool for different languages such as C(++) and Java",
     license="MIT",
+    entry_points={
+        'console_scripts': [
+            "breeze = breeze:cli"
+        ]
+    },
+    author_email='mauro.balades@tutanota.com',
     author="mauro-balades",
     packages=find_packages(),
     install_requires=[],
     long_description=long_description,
     classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.8",
     ]
