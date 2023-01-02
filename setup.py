f192cb77128f8caf1b53645d0fb09dd8154ab5b2
mauro-balades <mauro.balades@tutanota.com>
2023-01-02 20:13:55 +0100

    changed directory for breeze

--- setup.py
+++ setup.py
@@ -8,18 +8,19 @@ except IOError:
 
 setup(
     name="breeze-build",
-    version="0.1.1",
+    version="0.1.2",
     description="Breeze is a light-weight, highly-customizable build tool for different languages such as C(++) and Java",
     license="MIT",
     entry_points={
         'console_scripts': [
-            "breeze = src:main"
+            "breeze = src.breeze:main"
         ]
     },
     author_email='mauro.balades@tutanota.com',
     author="mauro-balades",
-    packages=find_packages(),
     install_requires=[],
+    package_dir = {"": "src"},
+    packages = find_packages(where="src"),
     long_description=long_description,
     classifiers=[
         'Intended Audience :: Developers',
